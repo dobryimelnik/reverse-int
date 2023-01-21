df7e426bd8fad8126d91fccb319d247aa2346038
Oleg Marjankov <marjan.om84@gmail.com>
2023-01-21 17:15:58 +0300

    feat: project finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
-}
+    let res = String(n);
+    
+    res = res.split('');
+    res.reverse();
+
+    if(res[res.length - 1] == '-') {
+      res.pop();
+    }
+
+    res = res.join('');  
+    return Number(res);
+};
+
